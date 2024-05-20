d888f4593f706820648be9cabbc149eec20fb3db
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-20 15:38:29 +0000

    [book/editの作成]4-課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    #@books = @user.books.page(params[:page])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
-  resources :books, only: [:new, :create, :index, :show]
-  resources :users, only: [:show, :edit]
+  root to: 'homes#top'
+  get 'home/about', to: 'homes#about'
+  resources :books, only: [:new, :create, :show, :edit, :index, :update, :destroy]
+  resources :users, only: [:show, :edit, :index, :update]
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
