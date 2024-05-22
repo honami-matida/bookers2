0c406186d2fd8a108953d2eeac338a2b7f33d513
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-22 02:38:38 +0000

    [エラー修正]4-課題3

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
+  validates :name, length: { minimum: 2,maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
+
   validates :name, presence: true
 
   def get_profile_image(width, height)
