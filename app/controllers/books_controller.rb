2906521290cfdc37ab13845a55c1509054cfda60
honami-matida <yuzuyuzu202405@gmail.com>
2024-05-22 05:09:11 +0000

    [Finish]4-課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def new
     @book = Book.new
@@ -56,6 +57,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2,maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def get_profile_image(width, height)
     unless profile_image.attached?
